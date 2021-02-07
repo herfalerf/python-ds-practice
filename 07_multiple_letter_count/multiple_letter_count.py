9d9ca052a35a178e7a78895616863087187f676d
unknown <jpendy@gmail.com>
2021-02-07 15:02:11 -0600

    first commit

---
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -0,0 +1,16 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    results = {}
+
+    for keys in phrase:
+        results[keys] = results.get(keys,0) +1
+    
+    return results
+
